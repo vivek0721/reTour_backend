cb8fbac250e4bc44dc46f73ce3cde9bd59c0dcfa
Vivek Jaiswal <commune.vivek@gmail.com>
2025-01-20 20:10:11 +0530

    Commit

---
+++ db/cloudinary.js
@@ -0,0 +1,4 @@
+import { v2 } from "cloudinary";
+import fs from fs;
+
+v2.uploader.upload()
--- index.js
+++ index.js
@@ -6,3 +6,13 @@ import dbConnect from "./db/index.js";
 dotenv.config();
 dbConnect();
 
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
